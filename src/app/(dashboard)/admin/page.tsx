131f36264084058ea2f0c9a23a480452b5652065
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-07-01 18:04:46 +0530

    Added EventCalendar Component

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
 import { FinanceChart } from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 import React from "react"
@@ -37,7 +38,9 @@ const AdminPage = () => {
       </div>
 
       {/* Right Content */}
-      <div className='w-full lg:w-1/3'>RIGHT</div>
+      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
+        <EventCalendar />
+      </div>
     </div>
   )
 }
