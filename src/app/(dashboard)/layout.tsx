6a8aa2199b956655f20d004e1fcf87b258936ac3
Raslan Rizvi <raslanrizvi05@gmail.com>
2025-07-02 00:44:48 +0530

    Changed Logo Text to Bold

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,13 +11,13 @@ export default function DashboardLayout({
   return (
     <div className='h-screen flex'>
       {/* Left Part */}
-      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]'>
+      <div className='w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4'>
         <Link
           href='/'
           className='flex items-center justify-center lg:justify-start gap-2 p-4'
         >
           <Image src='/logo.png' alt='logo' width={32} height={32} />
-          <span className='hidden lg:block'>SchoolLanka</span>
+          <span className='hidden lg:block font-bold'>SchoolLanka</span>
         </Link>
         <Menu />
       </div>
